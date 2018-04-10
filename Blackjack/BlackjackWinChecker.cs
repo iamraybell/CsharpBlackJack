65b92f052ce778b35758a4f1ac581f66e48ad70a
v-aschin <v-aschin@MININT-19PV3Q4>
2018-04-10 16:38:44 -0700

    Added IWinner and BlackjackWinChecker interface and class to check for win conditions

---
+++ Blackjack/BlackjackWinChecker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blackjack.Enums;
+using Blackjack.Interfaces;
+
+namespace Blackjack {
+
+    /// <summary>
+    /// BlackjackWinChecker, checks for the win condition
+    /// </summary>
+    public class BlackjackWinChecker : IWinChecker
+    {
+        /// <summary>
+        /// Check if the hand total equals a win condition
+        /// </summary>
+        /// <param name="deck">The deck</param>
+        /// <param name="hand">The hand</param>
+        /// <returns></returns>
+        public bool IsWin(IDeck deck, IHand hand);
+    }
+}
---
+++ Blackjack/Interfaces/IWinner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Interfaces
+{
+    /// <summary>
+    /// IWinner, represents something that checks for a win condition
+    /// </summary>
+    public interface IWinner
+    {
+        /// <summary>
+        /// Check if the hand total equals a win condition
+        /// </summary>
+        /// <param name="deck">The deck</param>
+        /// <param name="hand">The hand</param>
+        /// <returns></returns>
+        bool IsWin(IDeck deck, IHand hand);
+    }
+}
