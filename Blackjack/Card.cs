6466df7b9de926bd666d7d25202935cadc00e3eb
Charles Dunbar <charlesdunbar@email.arizona.edu>
2018-04-10 14:58:34 -0700

    Updated CardValue enum, Wrote some CardTests

--- Blackjack/Card.cs
+++ Blackjack/Card.cs
@@ -7,25 +7,39 @@ using Blackjack.Enums;
 using Blackjack.Interfaces;
 
 namespace Blackjack {
+
+    /// <summary>
+    /// Represents a card for a deck of cards
+    /// </summary>
     public class Card : ICard {
         public CardSuit Suit { get; set; }
         public CardValue Value { get; set; }
         public bool IsHidden { get; set; }
+
+        public Card(CardSuit suit, CardValue value, bool isHidden = false) {
+            Suit = suit;
+            Value = value;
+            IsHidden = isHidden;
+        }
 
         /// <summary>
         /// Returns the corresponding int value of a card's Value enum
         /// </summary>
         /// <returns>Card's blackjack value as an int</returns>
         public int GetIntValue() {
-            return (int) Value;
+            return Value.GetIntValue();
         }
 
+        /// <summary>
+        /// Returns a string representation of the card, depending on if
+        /// the card 'isHidden'.
+        /// </summary>
+        /// <returns>String representation of card</returns>
         public override string ToString() {
             if (IsHidden)
                 return "[*]";
 
-            //return "[" + 
-            return base.ToString();
+            return "[" + Value.GetString() + "]";
         }
     }
 }
