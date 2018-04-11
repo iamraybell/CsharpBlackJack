69ce730ad1c03d178334c08fda258ff127609145
v-aschin <v-aschin@MININT-19PV3Q4>
2018-04-11 15:35:56 -0700

    Adding Deal and displaying HumaPlayer Cards

--- Blackjack/GameManager.cs
+++ Blackjack/GameManager.cs
@@ -3,8 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Blackjack.Enums;
+using Blackjack.Exceptions;
+using Blackjack.Interfaces;
 
 namespace Blackjack {
-    public class GameManager {
+    public class GameManager
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private IInputProvider iinputProvider;
+
+        private IOutputProvider ioutputProvider;
+
+        private ICard card;
+
+        private IDeck deck;
+
+        private List<IPlayer> players;
+
+
+        public void AssignInitialCardsToPlayer()
+        {
+            players[0].Hand.Cards.Add(deck.Draw());
+            players[0].Hand.Cards.Add(deck.Draw());
+        } 
+
+        public void AssignInitialCardsToDealer()
+        {
+            players[1].Hand.Cards.Add(deck.Draw());
+            players[1].Hand.Cards.Add(deck.Draw());
+        }
+
+        /// <summary>
+        /// After typing Deal, HumanPlayer is assigned 2 cards which are displayed along with their total on the screen
+        /// </summary>
+        public void TypeDealAndShowCards()
+        {
+            ioutputProvider.WriteLine("Type Deal to have cards dealt");
+            iinputProvider.Read();
+
+            AssignInitialCardsToPlayer();
+            AssignInitialCardsToDealer();
+
+            ioutputProvider.WriteLine($"Dealer: [{players[1].Hand.Cards[0].GetIntValue()}] [{players[1].Hand.Cards[1].GetIntValue()}]");
+            ioutputProvider.WriteLine($"{players[0].Name}: [{players[0].Hand.Cards[0].GetIntValue()}] [{players[0].Hand.Cards[1].GetIntValue()}] - {players[0].Hand.GetTotalValue(players[0].Hand.Cards)}");
+
+        }
+
     }
 }
