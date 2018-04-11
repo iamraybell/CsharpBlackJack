e735e7f767aaf767973ba6284f428f8a66f283d2
Shelly Tang (InConsulting) <v-sheta@microsoft.com>
2018-04-11 16:19:41 -0700

    created CreatePlayer method.

--- Blackjack/GameManager.cs
+++ Blackjack/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blackjack.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,48 @@ using System.Threading.Tasks;
 
 namespace Blackjack {
     public class GameManager {
+
+
+
+        /// <summary>
+        /// The output provider
+        /// </summary>
+        private IOutputProvider outputProvider;
+
+        /// <summary>
+        /// The input provider
+        /// </summary>
+        private IInputProvider inputProvider;
+
+        /// <summary>
+        /// The list of players, uses IPlayers.
+        /// </summary>
+        private List<IPlayer> players;
+
+        /// <summary>
+        /// Prompts user for name, creates player, and adds player to the list.
+        /// </summary>
+        private void CreatePlayer()
+        {
+            //prompt user for name
+            outputProvider.WriteLine("Player 1, Enter your name.");
+
+            var inputName = inputProvider.Read();
+
+            var player = new HumanPlayer();
+
+            //if input is null or empty, set to default Player 1
+            if (string.IsNullOrEmpty(inputName))
+            {
+                player.Name = "Player 1";
+            }
+            else
+            {
+                player.Name = inputName;
+            }
+
+            // add to player list
+            players.Add(player);
+        }
     }
 }
