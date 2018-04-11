f01cf417ae87206f6fd1aee4a9a5d3dcdeb43f76
v-rode <v-rode@MININT-O2MFUC4>
2018-04-10 17:36:04 -0700

    Added dealer draw method with unit tests

---
+++ Blackjack.Tests/ComputerDealerTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Blackjack.Interfaces;
+
+namespace Blackjack.Tests
+{
+    [TestClass]
+    public class ComputerDealerTests
+    {
+        [TestMethod]
+        public void TestDealerDraw()
+        {
+            //arrange
+            var card = new Mock<ICard>(MockBehavior.Strict);
+            
+            var deck = new Mock<IDeck>(MockBehavior.Strict);
+            var hand = new Mock<IHand>(MockBehavior.Strict);
+            //hand.Setup(x => x.AddCard())
+
+            //act
+
+            //assign
+
+        }
+    }
+}
