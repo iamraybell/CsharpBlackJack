5795fb5e9cab9be759bd89439680ff17673e2f31
v-aschin <v-aschin@MININT-19PV3Q4>
2018-04-10 19:58:28 -0700

    Completed IsWin function and tests for the same

--- Blackjack/Interfaces/IWinner.cs
+++ Blackjack/Interfaces/IWinner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Blackjack.Enums;
 
 namespace Blackjack.Interfaces
 {
@@ -17,6 +18,6 @@ namespace Blackjack.Interfaces
         /// <param name="hand1">The Dealer hand</param>
         /// <param name="hand2">The Player hand</param>
         /// <returns></returns>
-        bool IsWin(IHand hand1, IHand hand2);
+        WinState IsWin(IHand hand1, IHand hand2);
     }
 }
