873b22afa588a6add90f530b28b56cdee6c89050
Charles Dunbar <charlesdunbar@email.arizona.edu>
2018-04-11 17:10:03 -0700

    Added Bank.cs

---
+++ Blackjack/Bank.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack {
+    public class Bank {
+        private const int BLACKJACK_STARTING_AMOUNT = 2000;
+
+        public int Balance { get; set; }
+
+        public Bank() {
+            Balance = BLACKJACK_STARTING_AMOUNT;
+        }
+
+        public Bank(int balance) {
+            Balance = balance;
+        }
+
+        /// <summary>
+        /// Withdraws amount from bank
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public bool Withdraw(int amount) {
+            if (Balance < amount) {
+                return false;
+            }
+
+            Balance -= amount;
+            return true;
+        }
+
+        /// <summary>
+        /// Deposits money in the bank
+        /// </summary>
+        /// <param name="amount"></param>
+        public void Deposit(int amount) {
+            Balance += amount;
+        }
+    }
+}
