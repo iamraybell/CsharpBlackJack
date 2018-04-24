49ee029c2136171800484ff75545595db4cf5015
va-rabell <va-rabell@microsoft.com>
2018-04-24 15:50:11 -0700

    Add ICard.cs

---
+++ Blackjack/Interfaces/ICard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blackjack.Enums;
+
+namespace Blackjack.Interfaces
+{
+    public interface ICard
+    {
+        CardSuit Suit { get; }
+        CardName Name { get; }
+        int Value { get; set; }
+        bool IsHidden { get; set; }
+
+        /// <summary>
+        /// Returns the corresponding int value of a card's Value enum
+        /// </summary>
+        /// <returns>Card's value as an int</returns>
+        int GetIntValue();
+    }
+}
